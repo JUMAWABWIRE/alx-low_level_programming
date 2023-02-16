3cc05a1176d170fec1f06fad9f1f6dd11d562efd
jumawabwire <wjjuma@gmail.com>
2023-02-16 09:25:43 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
+
 /**
- * main - main block
- 
- * Return: o
- 
+ * main-main blocl
+ * Return:l
  */
 
-in main(void)
+int main(void)
 
-{ 
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+{
+	puts("\"Programming is like building a multilingual puzzle,\n")
+        return (0);
 }
