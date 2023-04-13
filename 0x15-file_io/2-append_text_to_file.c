6ae79ec0b75f1b16d81cf485bad7df53e2ff5800
JUMAWABWIRE <wjjuma@gmail.com>
2023-04-13 13:54:51 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,43 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * append_text_to_file - appendinds text at the end of the file.
- * @filename: the name of the file.
- * @text_content: String to add at the end of the file.
- * Return: 1, otherwise -1
+ * Auth: Juma Wabwire
+ * Desc: Append a txt file at the end of the a file string.
  */
-int append_text_to_file(constant char *filename, char *text_content)
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: The Filename.
+ * @text_content: The string to add the file.
+ *
+ * Return: If the file exists -1
+ * Othwersie -1 if it does not exist.          
+ */
+int append_text_to_file(const char *filename, char *text_content)
 {
-int fd;
-int nrd;
-int wr;
+	int fd;
+	int nrd;
+	int wr;
 
-if (filename == NULL)
-return (-1);
+	if (!filename)
+		return (-1);
 
-if (text_content)
-{
-for (nrd = 0; text_content[nrd]; nrd++)
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	;
-wr = write(fd, text_content, nrd);
+	if (fd == -1)
+		return (-1);
 
-if (filename == NULL)
-	return (-1);
-fd = open(filename, O_WRONLY | O_APPEND);
-if (fd == -1)
-	return (-1);
-wr = write(fd, text_content, nrd);
-if (wr == -1)
-return (-1);
-}
+	if (text_content)
+	{
+		for (nrd = 0; text_content[nrd]; nrd++);
 
-close(fd);
+		wr = write(fd, text_content, nrd);
 
-return (1);
+		if (wr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
 }
